dc79951a1ad579124aab4039a1bdb95688a69334
Lance Guyatt <lanceguyatt@gmail.com>
2023-05-19 12:56:13 +0100

    studio

---
+++ app/studio/layout.tsx
@@ -0,0 +1,14 @@
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body>
+        <main className="flex-auto">{children}</main>
+      </body>
+    </html>
+  )
+}
